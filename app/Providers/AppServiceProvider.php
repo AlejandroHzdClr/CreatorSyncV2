05eeec02fe8ddbb74556759409935d654c7f6d36
Alejandro <alehdzclr@gmail.com>
2025-05-10 21:55:41 +0100

    Https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,24 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Spatie\Permission\Middleware\RoleMiddleware;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
+    /* Register any application services. */
     public function register(): void
     {
-        //
+        $this->app->bind('role', function ($app) {
+            return new RoleMiddleware();
+        });
     }
 
-    /**
-     * Bootstrap any application services.
-     */
+    /* Bootstrap any application services. */
     public function boot(): void
     {
-        //
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
